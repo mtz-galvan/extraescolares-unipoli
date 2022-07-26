d858bd368542333d348a5521b28ba6cf5d381972
mtz-galvan <110046423+mtz-galvan@users.noreply.github.com>
2022-07-26 17:35:36 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+//initiliazations
+const app = express();
+require('./database');
+require('./config/passport');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//global variables
+app.use((req, res, next) =>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+})
+
+//routes
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
+app.use(require('./routes/admin'));
+app.use(require('./routes/teachers'));
+app.use(require('./routes/notificaciones'));
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+// server is listenning
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
+
+app.use(function(req, res, next) {
+    res.status(404).render('404');
+  });
