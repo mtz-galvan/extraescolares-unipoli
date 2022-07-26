e22e733a44e22f3266c003cf685889f2b118b8ef
mtz-galvan <110046423+mtz-galvan@users.noreply.github.com>
2022-07-26 18:08:28 -0500

    Delete database.js

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/extraescolares', {
-    
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.log(err));
